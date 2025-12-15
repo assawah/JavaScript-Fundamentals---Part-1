8f8d6085ba238097772540f43fecefd4d35574eb
assawah <assawah@github.com>
2025-12-15 16:29:00 +0200

    let, const, and var

--- script.js
+++ script.js
@@ -1,16 +1,27 @@
 //Values and Variables
-let countrys = "Egypt";
+
+/* let countrys = "Egypt";
 let continent  = "Africa";
+ */
 let population = 110000000;
 console.log(countrys);
 console.log(continent);
 console.log(population);
 // -----------------------
 //Data Types
-let isIsland = false;
+
+//let isIsland = false;
 let language;
 console.log(typeof isIsland);
 console.log(typeof population); 
 console.log(typeof language);
 console.log(typeof countrys);
 // -----------------------
+//let, const, and var
+
+language = 'Arabic';
+const country= "Egypt";
+const continent = "Africa";
+const isIsland = false;
+
+

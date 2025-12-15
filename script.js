73eb9cf5835d3fd3efc5538b8338dff2c443d9cf
assawah <assawah@github.com>
2025-12-15 16:17:22 +0200

    Data Types

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ console.log(countrys);
 console.log(continent);
 console.log(population);
 // -----------------------
+//Data Types
+let isIsland = false;
+let language;
+console.log(typeof isIsland);
+console.log(typeof population); 
+console.log(typeof language);
+console.log(typeof countrys);
+// -----------------------

fc759231192611f9e217358c7d50ac3a120a474f
assawah <assawah@github.com>
2025-12-15 16:12:28 +0200

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+//Values and Variables
+let countrys = "Egypt";
+let continent  = "Africa";
+let population = 110000000;
+console.log(countrys);
+console.log(continent);
+console.log(population);
+// -----------------------
